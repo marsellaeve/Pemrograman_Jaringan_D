17933d869a15dfea24c37ec1510fda9b60d51069
marsellaeve <marsella.eve@gmail.com>
2021-04-25 20:27:57 +0000

    server

--- progjar3/Tugas3/server.py
+++ progjar3/Tugas3/server.py
@@ -1,19 +1,21 @@
 import socket
 
-UDP_IP_ADDRESS = '192.168.122.25'
+UDP_IP_ADDRESS = '192.168.122.168'
 UDP_PORT = 5050
-filename = 'pic.png'
 
 serverSock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 serverSock.bind(((UDP_IP_ADDRESS,UDP_PORT)))
-
+file=0
+filename='image'+str(file)
+print(filename)
+file=file+1
 fp = open(filename,'wb+')
 ditulis=0
 counter=0
 while True:
     data, addr = serverSock.recvfrom(1024)
     counter=counter+len(data)
-    print(addr," blok ", counter,"panjang : ",len(data), data)
+    #print(addr," blok ", counter,"panjang : ",len(data), data)
     fp.write(data)
 
 # while True:
